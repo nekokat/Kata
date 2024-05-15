b3c5e1ff6f168f7a6a70fb091cd757373f681f78
“nekokat” <nekokat89@gmail.com>
2024-05-15 20:57:29 +0300

    upd

---
+++ Brain.Fuck/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace BrainFuck;
+
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+        }
+    }
