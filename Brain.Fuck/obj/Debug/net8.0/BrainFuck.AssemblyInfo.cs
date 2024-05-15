94c6939d49cad8b49ca7f5022a9392d4936e702a
“nekokat” <nekokat89@gmail.com>
2024-05-15 20:39:56 +0300

    upd

--- Brain.Fuck/obj/Debug/net8.0/BrainFuck.AssemblyInfo.cs
+++ Brain.Fuck/obj/Debug/net8.0/BrainFuck.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrainFuck")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c220ad3c9428b7777a6f5d40bcb6f71b99ea1f7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a3d87629e1e243035702b5d346ab7b67660729")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrainFuck")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrainFuck")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Brain.Test/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Brain.Test/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c220ad3c9428b7777a6f5d40bcb6f71b99ea1f7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a3d87629e1e243035702b5d346ab7b67660729")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
